55377936eb668f22731cf7a4d821ad7583984823
micxdud <dudek_michal@hotmail.com>
2016-03-15 18:03:51 +0000

    Del shit

--- script.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.firstName = "Sample1";
-    $scope.lastName = "Sample2";
-});
